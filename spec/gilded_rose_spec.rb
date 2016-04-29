f99983cb82b16405a8b81696c54d856878fefe22
Omar Shariff <omarshariffd@gmail.com>
2016-04-29 16:38:29 +0800

    Cleans up gilded_rose_spec copypasta from item_spec

--- spec/gilded_rose_spec.rb
+++ spec/gilded_rose_spec.rb
@@ -1,13 +1,6 @@
 require 'spec_helper'
  
 describe GildedRose do
- 
-  describe "#initialize" do
-    it "takes three parameters and should be an Item" do
-      new_item = Item.new("item_name","item_sell_in","item_quality")
-      expect(new_item).to be_an_instance_of Item
-    end
-  end
 
   describe "#update_quality" do
     context "Sulfuras" do
