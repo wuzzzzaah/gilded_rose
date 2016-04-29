fb8a8b6c35e99574dad70fb2f4ceb95c4ebae4be
Omar Shariff <omarshariffd@gmail.com>
2016-04-29 17:47:11 +0800

    Adds test for GildedRose#initialize

--- spec/gilded_rose_spec.rb
+++ spec/gilded_rose_spec.rb
@@ -2,6 +2,16 @@ require 'spec_helper'
  
 describe GildedRose do
 
+  describe "#initialize" do
+    it "initializes an array of Items and stores it in @items" do
+      item1 = Item.new("item_1","item_sell_in","item_quality")
+      item2 = Item.new("item_2","item_sell_in","item_quality")
+      items = [item1,item2]
+      gilded_rose = GildedRose.new(items)
+      expect(gilded_rose.instance_variable_get("@items")).to eq(items)
+    end
+  end
+
   describe "#update_quality" do
     context "Sulfuras" do
       it "does" do
